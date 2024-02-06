1eac3cff8bdd46859997bc16ef28dfacde26ed2f
Griffin Annshual <griffintbr@gmail.com>
2024-02-07 00:05:12 +0530

    chore: initialize a flutter project

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\Program Files\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\GRIFFIN SPACE\Android\flutter\gapp_flutter"
+export "FLUTTER_APPLICATION_PATH=C:\GRIFFIN SPACE\Android\flutter\todo_app"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
